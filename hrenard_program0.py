23195a26725c1afa61d9c34ab440a69cadef2269
Hunter Renard <renardhj@gmail.com>
2018-01-24 11:56:54 -0500

    progress on python code, added corpus.txt

---
+++ hrenard_program0.py
@@ -0,0 +1,41 @@
+import sys
+import re
+
+class ngram:
+	def __init__(self, txt):
+		self.unigramModel = []
+		self.bigramModel = []
+		self.trigramModel = []
+
+	def unigram(self):
+		pass
+
+	def bigram(self):
+		pass
+
+	def trigram(aelf):
+		pass
+
+def main():
+	# default settings
+	fileName = "corpus.txt"
+	nValue = 3
+	generatedWordCount = 100
+	
+	# Reading in lines from file
+	f = open(fileName, "r")
+	fileOutput = f.read()
+	
+	# Cleaning file - removing special characters and lower casing all characters
+	cleanFileOutput = re.sub(r"[^a-zA-Z0-9]+", ' ', fileOutput).lower()
+
+	#ngram generation
+	ng = ngram(cleanFileOutput)
+
+	#unigram
+
+	#bigram
+
+	#trigram
+	
+main()
