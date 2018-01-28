9f720eab1430443036dc24cece1a4f4ff72ebff8
Hunter Renard <renardhj@gmail.com>
2018-01-28 10:10:40 -0500

    Adding brigram functionaity

--- hrenard_program0.py
+++ hrenard_program0.py
@@ -1,7 +1,6 @@
 import sys
 import re
 import numpy
-from itertools import islice
 
 class ngram:
 	def __init__(self, txt, count):
@@ -10,23 +9,36 @@ class ngram:
 		self.unigramModel = list(corpus)
 		self.bigramModel = list(zip(corpus, corpus[1:]))
 		self.trigramModel = list(zip(corpus, corpus[1:], corpus[2:]))
-
+		
 		self.count = count
 
 	def unigram(self):
 		#randomly selects a word from corpus.
 		output = []
+		
 		for i in range(self.count):
 			output.append(numpy.random.choice(self.unigramModel))
-		output = ' '.join(output).capitalize()
-		output = output + '.'
-		print(output)
+		
+		output = ' '.join(output).capitalize() + '.'
+		
+		return(output)
 
 	def bigram(self):
 		# selects a word based off of the previous word.
-		pass
+		output = []
 
-	def trigram(aelf):
+		prevWord = numpy.random.choice(self.unigramModel)
+		output.append(prevWord)
+
+		for i in range(self.count):
+			prevWord = numpy.random.choice([e[1] for e in self.bigramModel if e[0] == prevWord])
+			output.append(prevWord)
+
+		output = ' '.join(output).capitalize() + '.'
+		
+		return(output)
+
+	def trigram(self):
 		# selects a word based off of the previous two words.
 		pass
 
@@ -47,13 +59,14 @@ def main():
 
 	# Unigram
 	print("Unigram:")
-	ng.unigram()
+	print(ng.unigram())
 
 	# Bigram
 	print("\nBigram:")
-	ng.bigram()
+	print(ng.bigram())
 
 	# Trigram
 	print("\nTrigram:")
 	ng.trigram()
+
 main()
